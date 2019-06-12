7a8ebc7b303d61571f13179d5c114aab0ce8120b
Philip Crockett <pcrockett@pathwayservices.com>
2019-06-12 08:22:57 -0500

    Error handling in install script

--- Install.ps1
+++ Install.ps1
@@ -57,27 +57,43 @@ if (!(Test-Path $logDir)) {
 $serviceCreds = Get-Credential -Message "Enter credentials under which the service should run, or press cancel to run as LOCALSYSTEM (not recommended)."
 
 Invoke-Nssm install $ServiceName "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
-Invoke-Nssm set $ServiceName AppParameters "-NoProfile -ExecutionPolicy RemoteSigned ""$scriptPath"" -Url ""$UrlToMonitor"" -CronitorUrl ""$CronitorUrl"""
-Invoke-Nssm set $ServiceName AppDirectory C:\Windows\System32\WindowsPowerShell\v1.0
-Invoke-Nssm set $ServiceName AppExit "Default" "Exit"
-Invoke-Nssm set $ServiceName AppExit "1" "Exit"
-Invoke-Nssm set $ServiceName AppStdout $logFile
-Invoke-Nssm set $ServiceName AppStdoutCreationDisposition 2
-Invoke-Nssm set $ServiceName AppStderr $logFile
-Invoke-Nssm set $ServiceName AppStderrCreationDisposition 2
-Invoke-Nssm set $ServiceName AppRotateFiles 1
-Invoke-Nssm set $ServiceName AppRotateOnline 1
-Invoke-Nssm set $ServiceName AppRotateBytes 524288
-Invoke-Nssm set $ServiceName Description "An NSSM-hosted service running $scriptPath"
-Invoke-Nssm set $ServiceName DisplayName $ServiceName
-Invoke-Nssm set $ServiceName Start SERVICE_DELAYED_AUTO_START
-Invoke-Nssm set $ServiceName Type SERVICE_WIN32_OWN_PROCESS
 
-if ($serviceCreds) {
-    # This is the simplest way to get the plain-text password that I'm aware of:
-    $serviceCreds = New-Object System.Net.NetworkCredential `
-        -ArgumentList $serviceCreds.UserName, $serviceCreds.Password
+try {
 
-    $plaintextPassword = $serviceCreds.Password
-    Invoke-Nssm set $ServiceName ObjectName $serviceCreds.UserName $plaintextPassword
+    Invoke-Nssm set $ServiceName AppParameters "-NoProfile -ExecutionPolicy RemoteSigned ""$scriptPath"" -Url ""$UrlToMonitor"" -CronitorUrl ""$CronitorUrl"""
+    Invoke-Nssm set $ServiceName AppDirectory C:\Windows\System32\WindowsPowerShell\v1.0
+    Invoke-Nssm set $ServiceName AppExit "Default" "Exit"
+    Invoke-Nssm set $ServiceName AppExit "1" "Exit"
+    Invoke-Nssm set $ServiceName AppStdout $logFile
+    Invoke-Nssm set $ServiceName AppStdoutCreationDisposition 2
+    Invoke-Nssm set $ServiceName AppStderr $logFile
+    Invoke-Nssm set $ServiceName AppStderrCreationDisposition 2
+    Invoke-Nssm set $ServiceName AppRotateFiles 1
+    Invoke-Nssm set $ServiceName AppRotateOnline 1
+    Invoke-Nssm set $ServiceName AppRotateBytes 524288
+    Invoke-Nssm set $ServiceName Description "An NSSM-hosted service running $scriptPath"
+    Invoke-Nssm set $ServiceName DisplayName $ServiceName
+    Invoke-Nssm set $ServiceName Start SERVICE_DELAYED_AUTO_START
+    Invoke-Nssm set $ServiceName Type SERVICE_WIN32_OWN_PROCESS
+    
+    if ($serviceCreds) {
+        # This is the simplest way to get the plain-text password that I'm aware of:
+        $serviceCreds = New-Object System.Net.NetworkCredential `
+            -ArgumentList $serviceCreds.UserName, $serviceCreds.Password
+    
+        $plaintextPassword = $serviceCreds.Password
+        Invoke-Nssm set $ServiceName ObjectName $serviceCreds.UserName $plaintextPassword
+    }
+
+} catch {
+    # Some error has occurred. Let's remove the half-configured service so the
+    # user can re-run the install script more easily.
+
+    try {
+        Invoke-Nssm remove $ServiceName confirm | Out-Null
+    } catch {
+        Write-Warning "Unable to remove the half-configured service. Resolve the problem, then run ""nssm remove $ServiceName confirm"" before re-running the install script."
+    }
+
+    throw
 }
